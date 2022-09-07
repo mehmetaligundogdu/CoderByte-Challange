81fae246a6b82df34404705c64103ffa097b3eaa
Mehmet Ali <gundogdu.102@outlook.com>
2022-09-07 19:25:21 +0300

    Solution of Longest Word problem

---
+++ CoderByte-Challange/coderbyte-2.js
@@ -0,0 +1,23 @@
+//****************************************Longest Word************************************
+/*
+-Using the JavaScript language, have the function longestWord(sen) take the sen parameter being passed and return the largest word in the string.
+-If there are two or more words that are the same length, return the first word from the string with that length.
+-Ignore punctuation and assume sen will not be empty
+*/
+
+export const longestWord=function (sen){
+    const arr=sen.replace(/[^a-zA-Z ]/g, "").split(" "); //Regular expression kullanarak noktalama işaretlerini kelimelerden kaldırdık ve split yardımı ile boşluklardan kelimeleri bölerek Array oluşturduk.
+    arr.sort(function(a,b){ //sort metodunu fonksiyon olarak kullandık
+        return b.length-a.length; //uzunluk olarak büyük olanı ,arrayın başına geçirir.
+    })
+    console.log(arr);
+    return arr[0]; //Bize en uzun kelime lazım. Kelimeler uzunlukları arasında büyükten küçüğe sıralandıkları için en büyük kelime 0.indexe yerleşir.
+}
+
+//Noktalama işaretlerini kaldır
+
+//Kelimeleri boşluklardan split edip bir Array oluşturmak.
+
+//Oluşan Arrayı , kelimelerin karakter sayılarına göre büyükten küçüğe sıralamak (.sort() metodu) Sort; string array elemanlarının baş haflerini alfabetik olarak sıralar.Burda kelimelerin .length ini yani bir say kullanacağız. O sebeple .sort bir function olur.
+
+//En uzun kelimeyi (0.index) return etmek.
---
+++ solution.js
@@ -0,0 +1,6 @@
+import {userValidation} from  "./CoderByte-Challange/coderbyte-1.js";
+// console.log(userValidation("u__hello_world123"));
+
+import {longestWord} from "./CoderByte-Challange/coderbyte-2.js";
+
+console.log(longestWord("fun&!! time"));
